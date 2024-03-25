af6c8de176283430f5ce2b15c74a65e77873771e
Samuel Vinícius Basílio <samuelvinib@gmail.com>
2024-03-25 14:15:24 -0300

    first commit"

---
+++ src/app/diary/entry-item/entry-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-entry-item',
+  templateUrl: './entry-item.component.html',
+  styleUrls: ['./entry-item.component.css']
+})
+export class EntryItemComponent {
+
+}
