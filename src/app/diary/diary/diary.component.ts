962077474a3d658dceee8ba811e73ed599c47c07
Samuel Vinícius Basílio <samuelvinib@gmail.com>
2024-03-25 17:13:18 -0300

    chore: trackBy added to my diary list

--- src/app/diary/diary/diary.component.ts
+++ src/app/diary/diary/diary.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
-import { ExerciseSetList } from '../interfaces/exercise-set';
+import { ExerciseSet, ExerciseSetList } from '../interfaces/exercise-set';
 
 @Component({
   selector: 'app-diary',
   templateUrl: './diary.component.html',
-  styleUrls: ['./diary.component.css']
+  styleUrls: ['./diary.component.css'],
 })
 export class DiaryComponent {
-  exerciseList:ExerciseSetList=[
-  {id:'1', date: new Date(), exercise:'Deadlift', reps:15, sets:3},
-  {id:'2', date: new Date(), exercise:'Squat', reps:15, sets:3},
-  {id:'3', date: new Date(), exercise:'Barbell row', reps:15, sets:3},
+  exerciseList: ExerciseSetList = [
+    { id: '1', date: new Date(), exercise: 'Deadlift', reps: 15, sets: 3 },
+    { id: '2', date: new Date(), exercise: 'Squat', reps: 15, sets: 3 },
+    { id: '3', date: new Date(), exercise: 'Barbell row', reps: 15, sets: 3 },
   ];
+  newList() {
+    this.exerciseList = [
+      { id: '1', date: new Date(), exercise: 'Deadlift', reps: 15, sets: 3 },
+      { id: '2', date: new Date(), exercise: 'Squat', reps: 15, sets: 3 },
+      { id: '3', date: new Date(), exercise: 'Barbell row', reps: 15, sets: 3 },
+      { id: '4', date: new Date(), exercise: 'Leg Press', reps: 15, sets: 3 },
+    ];
+  }
+  itemTrackBy(index:number, item:ExerciseSet){
+    console.log(`retornou kkkkkkkkkkkkk o ${item.id}`)
+    return item.id;
+  }
 }
